3039730b312f9dd358adbf78a60b503b87753b61
jessiejiang1997 <xiaoyu_jiang@brown.edu>
2019-04-05 12:14:50 -0400

    remove extra comments

--- visual_analysis.py
+++ visual_analysis.py
@@ -40,57 +40,6 @@ class VisualAnalysis(object):
             ax = sns.scatterplot(x=i, y="immunization_rate", data=data)
             plt.savefig('./visualization/' + i + '_scatterplot.png')
 
-    #
-    #
-    # def plot_distributions(self):
-    #     # plot k12 enrollment of schools
-    #     enrollment = self.df_school['k12_enrollment'].values
-    #     fig, ax = plt.subplots()
-    #     ax.set_title('k12 enrollment')
-    #     ax.hist(enrollment, bins=50)
-    #     plt.savefig('./data_plots/k12_enrollment_distribution.png')
-    #
-    #     # plot percentage of all_immunizations
-    #     percentage = (self.df_school['all_immunizations'] / self.df_school['k12_enrollment']).values
-    #     fig, ax = plt.subplots()
-    #     ax.set_title('immunization rate')
-    #     ax.hist(percentage, bins=50)
-    #     plt.savefig('./data_plots/immunization_rate_distribution.png')
-    #
-    #     # plot county info
-    #     county_info = self.df_county.columns.values[2:]
-    #     bins = [9, 12, 20, 20, 20, 20, 20, 20, 20, 20, 20]
-    #     pair = zip(county_info, bins)
-    #     for column, num_bin in pair:
-    #         data = self.df_county[column].values
-    #         fig, ax = plt.subplots()
-    #         ax.set_title(column)
-    #         ax.hist(data, bins=num_bin, ec='black')
-    #         plt.savefig('./data_plots/' + column + '_distribution.png')
-    #
-    #
-    # def plot_correlation_heatmap(self):
-    #     # correlation among county's independent variables
-    #     fig, ax = plt.subplots(figsize=(20, 10))
-    #     sns.set()
-    #     county_info = self.df_county.columns.values[2:]
-    #     f = self.df_county.loc[:, county_info].corr()
-    #     ax = sns.heatmap(f, annot=True)
-    #     plt.subplots_adjust(left=0.25, bottom=0.36, right=1.00,
-    #                         top=0.98)
-    #     plt.savefig('./data_plots/county_correlation_heatmap.png')
-    #
-    # def describe_school(self):
-    #     df_school_info = self.df_school[['k12_enrollment', 'all_immunizations']]
-    #     print(df_school_info.describe())
-    #
-    # def describe_county(self):
-    #     # for printing on the command line
-    #     df_county_info_1 = self.df_county[self.df_county.columns[2:8]]
-    #     df_county_info_2 = self.df_county[self.df_county.columns[8:]]
-    #     print(df_county_info_1.describe())
-    #     print(df_county_info_2.describe())
-
 def main():
     va = VisualAnalysis()
     va.scatter_plot_county()
