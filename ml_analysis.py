54788961ea254a99e8c25d84a7f34de17f582a29
jessiejiang1997 <xiaoyu_jiang@brown.edu>
2019-04-16 23:46:15 -0400

    add more ML analysis (SVM, neural networks w/ feature selection)

--- ml_analysis.py
+++ ml_analysis.py
@@ -4,11 +4,14 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
-from sklearn import linear_model
+from sklearn import dummy, linear_model, svm
 from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
 from scipy import stats
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.feature_selection import SelectKBest, f_regression, mutual_info_regression
+from sklearn.neural_network import MLPRegressor
 
 
 all_features = ['rural_urban', 'urban_influence', 'high_school_degree_percent',
@@ -20,9 +23,13 @@ all_features = ['rural_urban', 'urban_influence', 'high_school_degree_percent',
 multi_regression_features_shortened = ['high_school_degree_percent',
                                        'percent_pov_child',
                                        'employment_rate',
+                                       'med_household_income_2017',
                                        'white_county',
                                        'white_school',
+                                       'grade_span',
+                                       'num_free_reduced_meals',
                                        'total_percent_absences',
+                                       'low_income_absence_percent',
                                        'school_district']
 
 labels = ['immunization_rate']
@@ -61,33 +68,51 @@ def preprocess_features():
     df['immunization_rate'] = (df['all_immunizations'] / df['k12_enrollment']).multiply(100).round(1)
     return df[all_features + labels]
 
+def feature_selection(df, method, k):
+    X = df[all_features].values
+    y = np.ravel(df[labels].values)
+    selector = SelectKBest(method, k=k)
+    X_selected = selector.fit_transform(X, y)
+    feature_indices = selector.get_support(indices=True)
+    selected_features = []
+    for index in feature_indices:
+        selected_features.append(all_features[index])
+    print(selected_features)
+    return X_selected
+
 def singleLinearRegression(df):
     for feature in all_features:
-        X = df[feature].values
-        X = np.reshape(X, (-1,1))
-        y = df[labels].values
-        X_train, X_test, y_train, y_test = train_test_split(
-            X,
-            y,
-            test_size=0.1,
-            shuffle=True
-        )
+        single_r_square = 0
+        single_train_mse = 0
+        single_test_mse = 0
+        for i in range(5):
+            X = df[feature].values
+            X = np.reshape(X, (-1,1))
+            y = df[labels].values
+            X_train, X_test, y_train, y_test = train_test_split(
+                X,
+                y,
+                test_size=0.1,
+                shuffle=True
+            )
 
-        lm = LinearRegression().fit(X_train, y_train)
+            lm = LinearRegression().fit(X_train, y_train)
 
-        train_r_squared = lm.score(X_train, y_train)
-        train_mse = mean_squared_error(y_train, lm.predict(X_train))
-        test_mse = mean_squared_error(y_test, lm.predict(X_test))
+            train_r_squared = lm.score(X_train, y_train)
+            train_mse = mean_squared_error(y_train, lm.predict(X_train))
+            test_mse = mean_squared_error(y_test, lm.predict(X_test))
+            single_r_square += train_r_squared
+            single_train_mse += train_mse
+            single_test_mse += test_mse
         print(feature + " results:")
-        print('Intercept: %f, Coefficient: %f' % (lm.intercept_,lm.coef_))
-        print('Training R-Squared: %f' % train_r_squared)
-        print('Training MSE: %f' % train_mse)
-        print('Testing MSE: %f' % test_mse)
+        print('Training R-Squared: %f' % (single_r_square / 5))
+        print('Training MSE: %f' % (single_train_mse / 5))
+        print('Testing MSE: %f' % (single_test_mse / 5))
 
-def multipleLinearRegression(df):
-    # change all_features to different feature list for different ML analysis
+def getMultipleLinearRegressionP(df):
     X = df[all_features].values
     y = df[labels].values
+
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
@@ -112,61 +137,159 @@ def multipleLinearRegression(df):
     myDF3["Coefficients"],myDF3["Standard Errors"],myDF3["t values"],myDF3["Probabilites"] = [params,sd_b,ts_b,p_values]
     print(myDF3)
 
-    train_r_squared = lm.score(X_train, y_train)
-    train_mse = mean_squared_error(y_train, lm.predict(X_train))
-    test_mse = mean_squared_error(y_test, lm.predict(X_test))
-    print('Training R-Squared: %f' % train_r_squared)
-    print('Training MSE: %f' % train_mse)
-    print('Testing MSE: %f' % test_mse)
+def multipleLinearRegression(X, y):
+    multi_r_sqaure = 0
+    multi_train_mse = 0
+    multi_test_mse = 0
+    for i in range(20):
+        X_train, X_test, y_train, y_test = train_test_split(
+            X,
+            y,
+            test_size=0.1,
+            shuffle=True
+        )
 
+        lm = LinearRegression().fit(X_train, y_train)
 
-def linearCrossTerms(df):
-    # change all_features to different feature list for different ML analysis
-    X = df[all_features].values
-    y = df[labels].values
-    X_train, X_test, y_train, y_test = train_test_split(
-        X,
-        y,
-        test_size=0.1,
-        shuffle=True
-    )
+        train_r_squared = lm.score(X_train, y_train)
+        train_mse = mean_squared_error(y_train, lm.predict(X_train))
+        test_mse = mean_squared_error(y_test, lm.predict(X_test))
+        multi_r_sqaure += train_r_squared
+        multi_train_mse += train_mse
+        multi_test_mse += test_mse
 
-    poly = PolynomialFeatures(interaction_only=True)
-    transformed = poly.fit_transform(X_train)
-    lm = linear_model.LinearRegression().fit(transformed, y_train)
+    print('Training R-Squared: %f' % (multi_r_sqaure / 20))
+    print('Training MSE: %f' % (multi_train_mse / 20))
+    print('Testing MSE: %f' % (multi_test_mse / 20))
 
-    params = np.append(lm.intercept_, lm.coef_)
-    predictions = lm.predict(transformed)
+def svm(X, y, kernel):
+    svm_r_sqaure = 0
+    svm_train_mse = 0
+    svm_test_mse = 0
+    for i in range(3):
+        X_train, X_test, y_train, y_test = train_test_split(
+            X,
+            y.ravel(),
+            test_size=0.1,
+            shuffle=True
+        )
 
-    newX = np.append(np.ones((len(transformed), 1)), transformed, axis=1)
-    MSE = (sum((y_train - predictions) ** 2)) / (len(newX) - len(newX[0]))
+        clf = SVR(kernel=kernel,
+                  gamma='auto').fit(X_train, y_train)
 
-    var_b = MSE * (np.linalg.pinv(np.dot(newX.T, newX)).diagonal())
-    sd_b = np.sqrt(var_b)
-    ts_b = params / sd_b
+        train_r_squared = clf.score(X_train, y_train)
+        train_mse = mean_squared_error(y_train, clf.predict(X_train))
+        test_mse = mean_squared_error(y_test, clf.predict(X_test))
+        svm_r_sqaure += train_r_squared
+        svm_train_mse += train_mse
+        svm_test_mse += test_mse
 
-    p_values = [2 * (1 - stats.t.cdf(np.abs(i), (len(newX) - 1))) for i in ts_b]
+    print('Training R-Squared: %f' % (svm_r_sqaure / 3))
+    print('Training MSE: %f' % (svm_train_mse / 3))
+    print('Testing MSE: %f' % (svm_test_mse / 3))
 
-    myDF3 = pd.DataFrame()
-    myDF3["Coefficients"],myDF3["Standard Errors"],myDF3["t values"],myDF3["Probabilites"] = [params,sd_b,ts_b,p_values]
-    print(myDF3)
+def neural_network(X, y):
+    nn_r_sqaure = 0
+    nn_train_mse = 0
+    nn_test_mse = 0
+    for i in range(5):
+        X_train, X_test, y_train, y_test = train_test_split(
+            X,
+            y.ravel(),
+            test_size=0.1,
+            shuffle=True
+        )
 
-    train_r_squared = lm.score(transformed, y_train)
-    train_mse = mean_squared_error(y_train, lm.predict(transformed))
-    test_transformed = poly.fit_transform(X_test)
-    test_mse = mean_squared_error(y_test, lm.predict(X_test))
-    print('Training R-Squared: %f' % train_r_squared)
-    print('Training MSE: %f' % train_mse)
-    print('Testing MSE: %f' % test_mse)
+        clf = MLPRegressor(hidden_layer_sizes=(8,),
+                           activation="tanh",
+                           alpha=0.001,
+                           learning_rate_init=0.001,
+                           max_iter=2000).fit(X_train, y_train)
+
+        train_r_squared = clf.score(X_train, y_train)
+        train_mse = mean_squared_error(y_train, clf.predict(X_train))
+        test_mse = mean_squared_error(y_test, clf.predict(X_test))
+        nn_r_sqaure += train_r_squared
+        nn_train_mse += train_mse
+        nn_test_mse += test_mse
+
+    print('Training R-Squared: %f' % (nn_r_sqaure / 5))
+    print('Training MSE: %f' % (nn_train_mse / 5))
+    print('Testing MSE: %f' % (nn_test_mse / 5))
+
+def baseLine(df):
+    dummy_train_r2 = 0
+    dummy_train_mse = 0
+    dummy_test_mse = 0
+    for i in range(5):
+        base_line_predictor = dummy.DummyRegressor(strategy="mean")
+        X = df[all_features].values
+        y = df[labels].values
+        X_train, X_test, y_train, y_test = train_test_split(
+            X,
+            y,
+            test_size=0.1,
+            shuffle=True
+        )
+
+        base_line_predictor.fit(X_train, y_train)
+        train_r_squared = base_line_predictor.score(X_train, y_train)
+        train_mse = mean_squared_error(y_train, base_line_predictor.predict(X_train))
+        test_mse = mean_squared_error(y_test, base_line_predictor.predict(X_test))
+        dummy_train_r2 += train_r_squared
+        dummy_train_mse += train_mse
+        dummy_test_mse += test_mse
+    print('Training R-Squared: %f' % (dummy_train_r2 / 5))
+    print('Training MSE: %f' % (dummy_train_mse / 5))
+    print('Testing MSE: %f' % (dummy_test_mse / 5))
 
 
 def main():
     df = preprocess_features()
-    print(df['immunization_rate'])
 
-    singleLinearRegression(df)
-    multipleLinearRegression(df)
-    #linearCrossTerms(df)
+    # baseLine(df)
+
+    # print('Top 1 Features selected by F-regression:')
+    # feature_selection(df, f_regression, 1)
+    # print('Top 3 Features selected by F-regression:')
+    # feature_selection(df, f_regression, 3)
+    print('Top 5 Features selected by F-regression:')
+    f_regression_X = feature_selection(df, f_regression, 5)
+    # print('Top 1 Features selected by mutual info regression:')
+    # feature_selection(df, mutual_info_regression, 1)
+    # print('Top 3 Features selected by mutual info regression:')
+    # feature_selection(df, mutual_info_regression, 3)
+    print('Top 5 Features selected by mutual info regression:')
+    mutual_info_X = feature_selection(df, mutual_info_regression, 5)
+
+    # singleLinearRegression(df)
+    # getMultipleLinearRegressionP(df)
+    # print('Multi-linear regression on all features:')
+    # multipleLinearRegression(df[all_features].values, df[labels].values)
+    # print('Multi-linear regression on features selected by p-values:')
+    # multipleLinearRegression(df[multi_regression_features_shortened].values, df[labels].values)
+    # print('Multi-linear regression on features selected by sklearn (f score):')
+    # multipleLinearRegression(f_regression_X, df[labels].values)
+    # print('Multi-linear regression on features selected by sklearn (mutual info):')
+    # multipleLinearRegression(mutual_info_X, df[labels].values)
+
+    # print('SVM (rbf) regression on all features:')
+    # svm(df[all_features].values, df[labels].values, 'rbf')
+    # print('SVM (rbf) regression on features selected by p-values:')
+    # svm(df[multi_regression_features_shortened].values, df[labels].values, 'rbf')
+    # print('SVM (rbf) regression on features selected by sklearn (f score):')
+    # svm(f_regression_X, df[labels].values, 'rbf')
+    # print('SVM (rbf) regression on features selected by sklearn (mutual info):')
+    # svm(mutual_info_X, df[labels].values, 'rbf')
+
+    # print('Neural Network regression on all features:')
+    # neural_network(df[all_features].values, df[labels].values)
+    # print('Neural Network regression on features selected by p-values:')
+    # neural_network(df[multi_regression_features_shortened].values, df[labels].values)
+    # print('Neural Network regression on features selected by sklearn (f score):')
+    # neural_network(f_regression_X, df[labels].values)
+    # print('Neural Network regression on features selected by sklearn (mutual info):')
+    # neural_network(mutual_info_X, df[labels].values)
 
 
 if __name__ == "__main__":
