fe7f2bd8680bb5750e0bba3db8c74abb46e95f47
huayu-ouyang <ouyang.huayu@gmail.com>
2019-05-01 10:24:32 -0400

    organizing this mess of a folder

---
+++ consolidate_data_scripts/absences_read.py
@@ -0,0 +1,53 @@
+import csv, sqlite3
+
+# Unexcused Absences Data
+# Data from http://reportcard.ospi.k12.wa.us/DataDownload.aspx
+
+def clean_school_name(name):
+    school = list(map(lambda x: x.lower(), name.split()))
+    if (school[-1] == 'school' or school[-1] == 'sch' or school[-1] == 'schl'):
+        del school[-1]
+    return " ".join(school)
+
+def main():
+    con = sqlite3.connect('washington.db')
+    cur = con.cursor()
+    #Notes - district name does not contain SCHOOL district
+    cur.execute('DROP TABLE IF EXISTS "absences";')
+    cur.execute("CREATE TABLE absences(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+    "district_code INTEGER, district TEXT, " +
+    "school_code INTEGER, school TEXT, total_percent_absences REAL," +
+    " low_income_absence_percent REAL);") # use your column names here
+
+    # District Code/District/School Code/School/Grade 1-8 Students/Low Income
+    # 14005/Aberdeen/2834/A J West Elementary/0.99%/1.23%
+
+    with open('unexcusedabsences16-17.csv','r', encoding ='latin-1') as fin: # `with` statement available in 2.5+
+        # csv.DictReader uses first line in file for column headings by default
+        dr = csv.DictReader(fin) # comma is default delimiter
+        to_db = [\
+        (i['District Code'], \
+        i['District'], \
+        i['School Code'], \
+        i['School'], \
+        i['Grade 1-8 Students'], \
+        i['Low Income']) for i in dr]
+
+        to_db_lst = list(to_db)
+        for row in to_db_lst:
+            row = list(row)
+            if (not (row[4] == 'n/a' or row[0] == '')):
+                row[1] = row[1].lower()
+                row[3] = row[3].lower()
+                row[3] = clean_school_name(row[3])
+                cur.execute("INSERT INTO absences(district_code, district, school_code, " +
+                "school, total_percent_absences, low_income_absence_percent)" +
+                " VALUES (?, ?, ?, ?, ?, ?);", row)
+
+        to_db = tuple(to_db_lst)
+
+    con.commit()
+    con.close()
+
+if __name__== "__main__":
+  main()
---
+++ consolidate_data_scripts/population_read.py
@@ -0,0 +1,64 @@
+import csv, sqlite3
+
+# Population Data
+# Data from https://www.ers.usda.gov/data-products/county-level-data-sets/download-data.aspx
+
+con = sqlite3.connect('washington.db')
+cur = con.cursor()
+cur.execute('DROP TABLE IF EXISTS "population";')
+cur.execute("CREATE TABLE population(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+"county TEXT, pop_chg_2015 INTEGER, pop_chg_2016 INTEGER, pop_chg_2017 INTEGER," +
+"birth_2015 INTEGER, birth_2016 INTEGER, birth_2017 INTEGER, int_mig_2015 INTEGER," +
+"int_mig_2016 INTEGER, int_mig_2017 INTEGER, dom_mig_2015 INTEGER," +
+" dom_mig_2016 INTEGER, dom_mig_2017 INTEGER);") # use your column names here
+
+with open('PopulationEstimates.csv','r', encoding ='latin-1') as fin: # `with` statement available in 2.5+
+    # csv.DictReader uses first line in file for column headings by default
+    dr = csv.DictReader(fin) # comma is default delimiter
+    to_db = [\
+    (i['State'], \
+    i['Area_Name'], \
+    i['N_POP_CHG_2015'], \
+    i['N_POP_CHG_2016'], \
+    i['N_POP_CHG_2017'], \
+    i['Births_2015'], \
+    i['Births_2016'], \
+    i['Births_2017'], \
+    i['INTERNATIONAL_MIG_2015'], \
+    i['INTERNATIONAL_MIG_2016'], \
+    i['INTERNATIONAL_MIG_2017'], \
+    i['DOMESTIC_MIG_2015'], \
+    i['DOMESTIC_MIG_2016'], \
+    i['DOMESTIC_MIG_2017']) for i in dr]
+
+    # [2] - Numeric Change in resident total population 7/1/2014 to 7/1/2015
+    # [3] - Numeric Change in resident total population 7/1/2015 to 7/1/2016
+    # [4] - Numeric Change in resident total population 7/1/2015 to 7/1/2017
+    # [5] -Births in period 7/1/2014 to 6/30/2015
+    # [6] -Births in period 7/1/2015 to 6/30/2016
+    # [7] -Births in period 7/1/2016 to 6/30/2017
+    # [8] - Net international migration in period 7/1/2014 to 6/30/2015
+    # [9] - Net international migration in period 7/1/2015 to 6/30/2016
+    # [10] - Net domestic migration in period 7/1/2014 to 6/30/2015
+    # [11] - Net domestic migration in period 7/1/2015 to 6/30/2016
+    # [12] - Net domestic migration in period 7/1/2016 to 6/30/2017
+
+    to_db_lst = list(to_db)
+    for row in to_db_lst:
+        row = list(row)
+        if row[0] == 'WA':
+            if row[1].endswith('County'):
+                del row[0]
+                row[0] = row[0][:-7]
+                print(row[0])
+                cur.execute("INSERT INTO population(" +
+                "county, pop_chg_2015, pop_chg_2016, pop_chg_2017," +
+                "birth_2015, birth_2016, birth_2017, int_mig_2015," +
+                "int_mig_2016, int_mig_2017, dom_mig_2015," +
+                " dom_mig_2016, dom_mig_2017)" +
+                " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", row)
+
+    to_db = tuple(to_db_lst)
+
+con.commit()
+con.close()
